2f3c7266b858007f5059f3f8cc6db7eb802791c1
Dirk Van Curan <dirkvancuran@gmail.com>
2019-11-05 08:36:02 -0800

    created flask app and organized repo

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, send_from_directory, render_template, request, abort
 from waitress import serve
-from models.wine_predictor import predict_wine
+from models.player_predictor import predict_player
 
 app = Flask(__name__, static_url_path="/static")
 
@@ -11,19 +11,25 @@ def index():
 
 @app.route("/get_results", methods=["POST"])
 def get_results():
-    """ Predict the class of wine based on the inputs. """
+    """ Predict the player class based on the inputs. """
     data = request.form
     print(data)
 
-    expected_features = ("Alcohol", "Malic acid", "Ash", "Alcalinity of ash",
-                         "Magnesium", "Total phenols", "Flavanoids", "Nonflavanoid phenols",
-                         "Proanthocyanins", "Color intensity", "Hue",
-                         "OD280/OD315 of diluted wines", "Proline")
+    expected_features = ('acceleration', 'sprint_speed', 'agility', 'balance',
+        'reactions', 'ball_control', 'composure', 'positioning',
+        'finishing','shot_power', 'long_shots', 'volleys', 'penalties',
+        'vision', 'crossing', 'free_kick', 'short_pass', 'long_pass',
+        'pass_curve', 'interceptions', 'heading', 'marking', 
+        'standing_tackle', 'sliding_tackle',
+        'jumping', 'stamina', 'strength', 'aggression')
+
+    for feature in expected_features:
+        print(data[feature])
 
     if data and all(feature in data for feature in expected_features):
         # Convert the dict of fields into a list
         test_value = [float(data[feature]) for feature in expected_features]
-        predicted_class = predict_wine(test_value)
+        predicted_class = predict_player(test_value)
         return render_template("results.html", predicted_class=predicted_class)
     else:
         return abort(400)
